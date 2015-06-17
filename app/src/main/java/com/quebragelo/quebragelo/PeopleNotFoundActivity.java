394ae2f970fe0575f1b4f2cfee635dbb841d1314
joaopaulo <jhonyvanin@gmail.com>
2015-06-17 12:17:31 -0300

    create SharedButton on layout people not found

--- app/src/main/java/com/quebragelo/quebragelo/PeopleNotFoundActivity.java
+++ app/src/main/java/com/quebragelo/quebragelo/PeopleNotFoundActivity.java
@@ -15,5 +15,9 @@ public class PeopleNotFoundActivity extends Activity {
 
         ImageView img = (ImageView) findViewById(R.id.imageView5);
         img.setImageResource(R.mipmap.notpeople);
+
+        
+
+
     }
 }
